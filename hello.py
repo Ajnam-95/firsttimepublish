7344e9898382240d03d0b2ea87af129287544768
Ajnam-95 <118440106+Ajnam-95@users.noreply.github.com>
2025-08-06 11:14:30 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,6 @@
+def say_hello(name):
+    """ A basic function that prints hello along with the users name. 
+    @param name A String with the name of the user in quotation marks."""
+
+    print("Hello, " + name)
+
